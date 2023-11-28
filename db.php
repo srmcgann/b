0c18aee694161b78fe3409c1c9d377fb1ea66e55
scott mcgann <s.r.mcgann@hotmail.com>
2023-11-28 01:17:29 +0000

    initial commit

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+  //error_reporting(0);
+  $db_user  = 'id21257390_user';
+  $db_pass  = 'Chrome57253!*';
+  $db_host  = 'localhost';
+  $db       = "id21257390_default";
+  $port     = '3306';
+  $link     = mysqli_connect($db_host,$db_user,$db_pass,$db,$port);
+
+  $baseURL  = "http://0.cantelope.org/b/games/tetris";
+  $link     = mysqli_connect($db_host,$db_user,$db_pass,$db,$port);
+?>
