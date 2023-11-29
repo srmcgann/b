f97baef858aa03044b802bed920ea8ab040c1efb
scott mcgann <s.r.mcgann@hotmail.com>
2023-11-29 22:08:12 +0000

    sync

--- db.php
+++ db.php
@@ -1,12 +1,9 @@
-<?php
-  //error_reporting(0);
-  $db_user  = 'id21257390_user';
-  $db_pass  = 'Chrome57253!*';
-  $db_host  = 'localhost';
-  $db       = "id21257390_default";
-  $port     = '3306';
-  $link     = mysqli_connect($db_host,$db_user,$db_pass,$db,$port);
-
-  $baseURL  = "http://0.cantelope.org/b/games/tetris";
-  $link     = mysqli_connect($db_host,$db_user,$db_pass,$db,$port);
+<?
+  $db_user="user";
+  $db_pass="";
+  $db_host="localhost";
+  $db="tetris";
+  $baseURL='demo.whitehotrobot.com';
+  $link = mysqli_connect($db_host, $db_user, $db_pass, $db);
 ?>
+
--- sync.php
+++ sync.php
@@ -1,11 +1,11 @@
-<?php
+<?
   require('db.php');
   $data = json_decode(file_get_contents('php://input'));
 	$gameID = mysqli_real_escape_string($link, $data->{'gameID'});
 	$playerNumber = mysqli_real_escape_string($link, $data->{'playerNumber'});
 	$playerName = mysqli_real_escape_string($link, $data->{'playerName'});
 	$receiveddata = $data->{'gamedata'};
-	$sql = 'SELECT * FROM games WHERE id = ' . $gameID;
+	$sql = 'SELECT * FROM tetrisGames WHERE id = ' . $gameID;
 	$res = mysqli_query($link, $sql);
 	$row = mysqli_fetch_assoc($res);
 	$gamefull = false;
@@ -24,16 +24,16 @@
 	$postdata = mysqli_real_escape_string($link, json_encode($receiveddata));
 	switch($playerNumber){
 	  case 'A':
-		  $sql = 'UPDATE games SET gamedataA = "' . $postdata . '" WHERE id = ' . $gameID;
+		  $sql = 'UPDATE tetrisGames SET gamedataA = "' . $postdata . '" WHERE id = ' . $gameID;
 		break;
     case 'B':
-      $sql = 'UPDATE games SET gamedataB = "' . $postdata . '" WHERE id = ' . $gameID;
+      $sql = 'UPDATE tetrisGames SET gamedataB = "' . $postdata . '" WHERE id = ' . $gameID;
     break;
     case 'C':
-      $sql = 'UPDATE games SET gamedataC = "' . $postdata . '" WHERE id = ' . $gameID;
+      $sql = 'UPDATE tetrisGames SET gamedataC = "' . $postdata . '" WHERE id = ' . $gameID;
     break;
     case 'D':
-      $sql = 'UPDATE games SET gamedataD = "' . $postdata . '" WHERE id = ' . $gameID;
+      $sql = 'UPDATE tetrisGames SET gamedataD = "' . $postdata . '" WHERE id = ' . $gameID;
     break;
 		default:
 		  $gamefull = true;
@@ -46,7 +46,7 @@
 	  	mysqli_query($link, $sql);
 	  }
 
-    $sql = 'SELECT * FROM games WHERE id = ' . $gameID;
+    $sql = 'SELECT * FROM tetrisGames WHERE id = ' . $gameID;
     $res = mysqli_query($link, $sql);
     $row = mysqli_fetch_assoc($res);
 
@@ -61,14 +61,14 @@
   			$gamedata = json_decode($row[$col]);
         if(key_exists('lastUpdate', $gamedata)){
           if(time() - $gamedata->{'lastUpdate'} > 10){
-            $sql = 'UPDATE games SET ' . $col . ' = "" WHERE id = ' . $gameID;
+            $sql = 'UPDATE tetrisGames SET ' . $col . ' = "" WHERE id = ' . $gameID;
 				  	mysqli_query($link, $sql);
 					}
         }
       }
     }
 
-    $sql = 'SELECT * FROM games WHERE id = ' . $gameID;
+    $sql = 'SELECT * FROM tetrisGames WHERE id = ' . $gameID;
     $res = mysqli_query($link, $sql);
     $row = mysqli_fetch_assoc($res);
 
